6f1e8e0ac1c7440f3aa4b8cfaa1c5f40951fda57
felix fedronic <fedronicfelix@gmail.com>
2023-02-23 22:02:56 +0100

    top navbar in progress

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,45 +1,210 @@
 import Link from "next/link";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
+
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import WalletIcon from "@mui/icons-material/Wallet";
+import AccountCircleIcon from "@mui/icons-material/AccountCircleRounded";
+import Box from "@mui/material/Box";
 
 function Layout(props) {
+  const pages = ["budgets", "transactions"];
+  const settings = ["profile", "logout"];
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
+  const [isAuth, setIsAuth] = useState(true);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
     <Fragment>
       <header>
-        <nav>
-          <ul>
-            <li>
-              <Link href={"/"}>Home</Link>
-            </li>
-            <li>
-              <Link href={"/budgets"}>Budgets</Link>
-            </li>
-            <li>
-              <Link href={"/transactions"}>Transactions</Link>
-            </li>
-            <li>
-              <Link href={"/sign-in"}>Sign-in</Link>
-            </li>
-            <li>
-              <Link href={"/sign-up"}>Sign-up</Link>
-            </li>
-          </ul>
-        </nav>
+        <AppBar position="static">
+          <Container maxWidth="xl">
+            <Toolbar disableGutters>
+              <WalletIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+              <Typography
+                variant="h6"
+                noWrap
+                component="div"
+                sx={{
+                  mr: 2,
+                  display: { xs: "none", md: "flex" },
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+              >
+                <Link href={"/"}> Budget Tracker</Link>
+              </Typography>
+
+              <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleOpenNavMenu}
+                  color="inherit"
+                >
+                  <MenuIcon />
+                </IconButton>
+                {isAuth && (
+                  <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorElNav}
+                    anchorOrigin={{
+                      vertical: "bottom",
+                      horizontal: "left",
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: "top",
+                      horizontal: "left",
+                    }}
+                    open={Boolean(anchorElNav)}
+                    onClose={handleCloseNavMenu}
+                    sx={{
+                      display: { xs: "block", md: "none" },
+                    }}
+                  >
+                    {pages.map((page) => (
+                      <MenuItem key={page} onClick={handleCloseNavMenu}>
+                        <Typography textAlign="center">
+                          <Link href={`/${page}`}>{page}</Link>
+                        </Typography>
+                      </MenuItem>
+                    ))}
+                  </Menu>
+                )}
+                {!isAuth && (
+                  <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorElNav}
+                    anchorOrigin={{
+                      vertical: "bottom",
+                      horizontal: "left",
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: "top",
+                      horizontal: "left",
+                    }}
+                    open={Boolean(anchorElNav)}
+                    onClose={handleCloseNavMenu}
+                    sx={{
+                      display: { xs: "block", md: "none" },
+                    }}
+                  >
+                    <MenuItem key={"sign-in"} onClick={handleCloseUserMenu}>
+                      <Typography textAlign="center">
+                        <Link href={"/sign-in"}>sign-in</Link>
+                      </Typography>
+                    </MenuItem>
+                    <MenuItem key={"sign-up"} onClick={handleCloseUserMenu}>
+                      <Typography textAlign="center">
+                        <Link href={"/sign-up"}>sign-up</Link>
+                      </Typography>
+                    </MenuItem>
+                  </Menu>
+                )}
+              </Box>
+              <WalletIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+              <Typography
+                variant="h6"
+                noWrap
+                component="div"
+                sx={{
+                  mr: 2,
+                  display: { xs: "flex", md: "none" },
+                  flexGrow: 1,
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+              >
+                <Link href={"/"}> Budget Tracker</Link>
+              </Typography>
+              <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+                {pages.map((page) => (
+                  <Button
+                    key={page}
+                    onClick={handleCloseNavMenu}
+                    sx={{ my: 2, color: "white", display: "block" }}
+                  >
+                    <Link href={`/${page}`}>{page}</Link>
+                  </Button>
+                ))}
+              </Box>
+              {isAuth && (
+                <Box sx={{ flexGrow: 0 }}>
+                  <Tooltip title="Open settings">
+                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                      <Avatar sx={{ width: 40, height: 40 }}>
+                        <AccountCircleIcon sx={{ width: 30, height: 30 }} />
+                      </Avatar>
+                    </IconButton>
+                  </Tooltip>
+                  <Menu
+                    sx={{ mt: "45px" }}
+                    id="menu-appbar"
+                    anchorEl={anchorElUser}
+                    anchorOrigin={{
+                      vertical: "top",
+                      horizontal: "right",
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: "top",
+                      horizontal: "right",
+                    }}
+                    open={Boolean(anchorElUser)}
+                    onClose={handleCloseUserMenu}
+                  >
+                    {settings.map((setting) => (
+                      <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                        <Typography textAlign="center">
+                          <Link href={`/${setting}`}>{setting}</Link>
+                        </Typography>
+                      </MenuItem>
+                    ))}
+                  </Menu>
+                </Box>
+              )}
+            </Toolbar>
+          </Container>
+        </AppBar>
       </header>
 
       <main>{props.children}</main>
 
-      <footer>
-        <nav>
-          <ul>
-            <li>
-              <Link href={"/budgets/add"}>Add A Budget</Link>
-            </li>
-            <li>
-              <Link href={"/transactions/add"}>Add A Transaction</Link>
-            </li>
-          </ul>
-        </nav>
-      </footer>
+      <footer></footer>
     </Fragment>
   );
 }
--- pages/transactions/index.js
+++ pages/transactions/index.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Fragment } from "react";
+import Head from "next/head";
+import { CssBaseline, Typography } from "@mui/material";
 
 function TransactionsPage() {
   return (
-    <div>TransactionsPage</div>
-  )
+    <Fragment>
+      <Head>
+        <title>TransactionsPage</title>
+        <meta name="description" content="list of transactions" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <CssBaseline />
+      <Typography variant="h6">TransactionsPage</Typography>
+    </Fragment>
+  );
 }
 
-export default TransactionsPage
+export default TransactionsPage;
